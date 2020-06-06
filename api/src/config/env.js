c315c48635b571c94364aa316102a693a40a88eb
ronan-f <rfegan13@gmail.com>
2020-06-05 22:43:40 -0700

    Updated env directory

--- api/src/config/env.js
+++ api/src/config/env.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
+require('dotenv').config({ path: __dirname + '/../../.env' });
 
 function getEnv(key) {
     if (!process.env[key]) {
-        throw Error(`Could not find environment variable with key: ${key}`)
+        throw Error(`Could not find environment variable with key: ${key}`);
     }
 
-    return process.env[key]
+    return process.env[key];
 }
 
 module.exports = {
     PORT: getEnv('PORT'),
-}
+};
