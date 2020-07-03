0987f792fd3ff9e11a429399275cd6f892d51baf
ronan-f <rfegan13@gmail.com>
2020-07-02 19:55:45 -0700

    Added migration

---
+++ migrations/20200620234427_create_tables.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('users', function (table) {
+        table.increments();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('password').notNullable();
+        table.string('reset_password_token').notNullable();
+        table.boolean('is_deleted').notNullable().defaultTo(false);
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('users');
+};
